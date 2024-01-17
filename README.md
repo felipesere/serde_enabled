8f451b98aaa02908d4affb8a91a9f7a68db95cee
Felipe Sere <github@felipesere.com>
2024-01-17 21:36:35 +0000

    Move the docs to README but include them back

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# serde_enabled
-A utility type to support deserializing things which can be enabled or disabled
+`Enable<T>` is a wrapper that adds one extra field `enabled` when the `T`
+is serialized with `Serde`. That field can be `true`, in which case all
+of the fields for `T` need to be present, or it can be `false` at which point
+all fields of `T` can be ommited.
+
+The use case is for configuration YAMLs where sections can be toggled on or off.
+
+```
+use serde::{Deserialize, Serialize};
+use serde_enabled::Enable;
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+struct Outside {
+    inside: Enable<Inside>,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+struct Inside {
+    thing: u32,
+    other: String,
+}
+
+let raw = indoc::indoc! {r#"
+    inside:
+        enable: false
+        thing: 1
+        other: "Great"
+    "#};
+
+let o: Outside = serde_yaml::from_str(raw).unwrap();
+
+ assert!(!o.inside.is_enabled());
+
+ let raw = indoc::indoc! {r#"
+     inside:
+         enable: true
+         thing: 1
+         other: "Great"
+     "#};
+
+ let o: Outside = serde_yaml::from_str(raw).unwrap();
+
+  assert!(o.inside.is_enabled());
+  assert_eq!(
+        o,
+        Outside {
+           inside: Enable::On(Inside {
+            thing: 1,
+            other: "Great".into()
+        })
+        }
+    );
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-/// `Enable<T>` is a wrapper that adds one extra field `enabled` when the `T`
-/// is serialized with `Serde`. That field can be `true`, in which case all
-/// of the fields for `T` need to be present, or it can be `false` at which point
-/// all fields of `T` can be ommited.
-///
-/// The use case is for configuration YAMLs where sections can be toggled on or off.
-/// ```
-/// use serde::{Deserialize, Serialize};
-/// use serde_enabled::Enable;
-///
-/// #[derive(Debug,Deserialize, Serialize, PartialEq, Eq)]
-/// struct Outside {
-///     inside: Enable<Inside>,
-/// }
-///
-/// #[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
-/// struct Inside {
-///     thing: u32,
-///     other: String,
-/// }
-///
-/// let raw = indoc::indoc! {r#"
-///     inside:
-///         enable: false
-///         thing: 1
-///         other: "Great"
-///     "#};
-///
-/// let o: Outside = serde_yaml::from_str(raw).unwrap();
-///
-///  assert!(!o.inside.is_enabled());
-///
-///  let raw = indoc::indoc! {r#"
-///      inside:
-///          enable: true
-///          thing: 1
-///          other: "Great"
-///      "#};
-///
-///  let o: Outside = serde_yaml::from_str(raw).unwrap();
-///
-///   assert!(o.inside.is_enabled());
-///   assert_eq!(
-///         o,
-///         Outside {
-///            inside: Enable::On(Inside {
-///             thing: 1,
-///             other: "Great".into()
-///         })
-///         }
-///     );
-/// ```
+#[doc = include_str!("../README.md")]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Enable<T> {
     On(T),
