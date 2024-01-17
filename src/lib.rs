4b7ee3e0d441264790d237e2642a2b297f431dd1
Felipe Sere <github@felipesere.com>
2024-01-17 21:33:17 +0000

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,58 @@
 use serde::{Deserialize, Serialize};
 
-/// `Enable<T>` is a wrapper to properly `Serialize` and `Deserialize`
-/// settings that can be turned `On` or `Off`.
-/// Particularly, in the `Off` variant the aditional fields are not required.
-#[derive(Clone, Debug)]
+/// `Enable<T>` is a wrapper that adds one extra field `enabled` when the `T`
+/// is serialized with `Serde`. That field can be `true`, in which case all
+/// of the fields for `T` need to be present, or it can be `false` at which point
+/// all fields of `T` can be ommited.
+///
+/// The use case is for configuration YAMLs where sections can be toggled on or off.
+/// ```
+/// use serde::{Deserialize, Serialize};
+/// use serde_enabled::Enable;
+///
+/// #[derive(Debug,Deserialize, Serialize, PartialEq, Eq)]
+/// struct Outside {
+///     inside: Enable<Inside>,
+/// }
+///
+/// #[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+/// struct Inside {
+///     thing: u32,
+///     other: String,
+/// }
+///
+/// let raw = indoc::indoc! {r#"
+///     inside:
+///         enable: false
+///         thing: 1
+///         other: "Great"
+///     "#};
+///
+/// let o: Outside = serde_yaml::from_str(raw).unwrap();
+///
+///  assert!(!o.inside.is_enabled());
+///
+///  let raw = indoc::indoc! {r#"
+///      inside:
+///          enable: true
+///          thing: 1
+///          other: "Great"
+///      "#};
+///
+///  let o: Outside = serde_yaml::from_str(raw).unwrap();
+///
+///   assert!(o.inside.is_enabled());
+///   assert_eq!(
+///         o,
+///         Outside {
+///            inside: Enable::On(Inside {
+///             thing: 1,
+///             other: "Great".into()
+///         })
+///         }
+///     );
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Enable<T> {
     On(T),
     Off,
@@ -49,6 +98,8 @@ impl<T: Serialize> Serialize for Enable<T> {
 }
 
 impl<T> Enable<T> {
+    /// Turns the `Enable` into an `Option` where `Enable::On(T)` maps to `Option::Some(T)` and
+    /// `Enable::Off` to `None`.
     pub fn into_inner(self) -> Option<T> {
         match self {
             Enable::On(inner) => Some(inner),
